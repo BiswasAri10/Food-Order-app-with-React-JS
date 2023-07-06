33d76ca8c1af11a0478ea2b1b048b513c1118d20
BiswasAri10 <bis.ips21@gmail.com>
2023-07-06 18:58:54 +0530

    Cart open and close functionality added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>FoodMart</h1>
-        <HeaderCartButton></HeaderCartButton>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious foods"></img>
